e77d2d09a3bd386cffaa2349ad279ea5672e09b2
maskface <you@example.com>
2026-08-18 18:12:49 +0100

    .

--- srcs/requirements/nginx/tools/init-nginx.sh
+++ srcs/requirements/nginx/tools/init-nginx.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+mkdir -p /etc/nginx/ssl /var/www/html /run/nginx
 
-mkdir -p /var/www/html /run/nginx
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/nginx/ssl/nginx.key \
+  -out /etc/nginx/ssl/nginx.crt \
+  -subj "/CN=${DOMAIN_NAME}" \
+  -addext "subjectAltName=DNS:${DOMAIN_NAME}"
 
 exec nginx -g 'daemon off;'
