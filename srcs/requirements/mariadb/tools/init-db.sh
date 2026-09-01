b161c0476b88cd6303330b6cd25351908dd8d5b0
maskface02 <you@example.com>
2026-09-01 07:35:29 +0100

    .

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB database..."
+  echo "Initializing MariaDB database..."
 
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    DB_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
-    ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+  DB_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
+  ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
 
-    mysqld --user=mysql --bootstrap << EOF
+  mysqld --user=mysql --bootstrap <<EOF
 USE mysql;
-FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
 EOF
 
-    echo "MariaDB initialized successfully."
+  echo "MariaDB initialized successfully."
 fi
 
 exec mysqld --user=mysql
